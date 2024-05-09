cd8bdba52c1d25e1be32e3db0520d7f28ed80807
Yonela986 <yoza.futho@gmail.com>
2024-05-09 11:14:20 +0200

    changes

--- servicepage.js
+++ servicepage.js
@@ -14,7 +14,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
 
   if (isEmpty) {
     const errorMessageElement = document.createElement("div");
-    errorMessageElement.className.add("error-message");
+    errorMessageElement.className = "error-message";
     errorMessageElement.textContent = "Please fill in all fields.";
     container.appendChild(errorMessageElement);
     console.log(errorMessageElement);
